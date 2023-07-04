75aa1f8e854fc6b57264e87fcb8abefbecdfd2fc
SH20RAJ <66713844+SH20RAJ@users.noreply.github.com>
2023-07-04 04:34:15 +0000

    il

--- sp.php
+++ sp.php
@@ -24,6 +24,24 @@ function replaceAllLinks($html) {
     $modifiedHtml = $dom->saveHTML();
     return $modifiedHtml;
 }
+function replaceLinksInAside($html) {
+    $dom = new DOMDocument();
+    libxml_use_internal_errors(true); // Disable error reporting
+    $dom->loadHTML($html);
+    libxml_clear_errors(); // Clear any existing errors
+
+    $xpath = new DOMXPath($dom);
+    $nodes = $xpath->query("//aside//a");
+
+    foreach ($nodes as $node) {
+        $href = $node->getAttribute('href');
+        $modifiedUrl = 'sp/' . ltrim($href, '/');
+        $node->setAttribute('href', $modifiedUrl);
+    }
+
+    $modifiedHtml = $dom->saveHTML();
+    return $modifiedHtml;
+}
 
 
 // Fetch HTML content from the URL
@@ -32,7 +50,7 @@ $html = file_get_contents($url);
 // Check if HTML content is successfully fetched
 if ($html !== false) {
     // Apply replacements
-    $modifiedHtml = replaceAllLinks($html);
+    $modifiedHtml = replaceLinksInAside(replaceAllLinks($html));
     
     // Output the modified HTML
     echo $modifiedHtml;
